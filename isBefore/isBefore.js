a30e383438bf69079746512a088b42efa811a86e
Ruslan <787ruslan@gmail.com>
2023-03-17 20:05:24 +0600

    isBefore

--- isBefore/isBefore.js
+++ isBefore/isBefore.js
@@ -1,12 +1,7 @@
 function isBefore(target, base) {
     let targetDate = new Date(target);
     let baseDate = new Date(base);
-    if (targetDate < baseDate) {
-        return(true);
-      } else {
-        return(false);
-      }
-     
+    return (targetDate < baseDate);
     }
 console.log(isBefore('01.01.2023', '01.02.2022')); // false
 console.log(isBefore('01.02.2022', '01.01.2023')) // true
